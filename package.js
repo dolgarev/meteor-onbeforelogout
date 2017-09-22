463aa241338bc61e3f79b7131b2e924773292628
dolgarev <o.dolgarev@gmail.com>
2017-09-22 10:57:00 +0300

    bump version to 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'liberation:meteor-onbeforelogout',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'This package realizes callback function onBeforeLogout.',
   // URL to the Git repository containing the source code for this package.
